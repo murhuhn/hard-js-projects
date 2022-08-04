b06219000a69bd6b5a39f620802ae100df34366d
murhuh <murhuh@inbox.ru>
2022-08-04 22:25:41 +0300

    Доделала усложненное задание из урока 3, добавила решения через switch-case и массив

--- script.js
+++ script.js
@@ -2,19 +2,49 @@
 
 // 1)
 
-while (true) {
-  lang = prompt("Установите язык, введя 'ru' или 'en' в поле ниже", "ru/en");
 
-  if (lang == 'ru') {
-    console.log('Понедельник, вторник, среда, четверг пятницы, суббота, воскресенье.');
-    break;
-  } else if (lang == 'en') {
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.');
-    break;
-  }
 
-  alert("Неправильное значение. Введите 'ru' или 'en'.");
+// while (true) {
+let lang = prompt("Установите язык, введя 'ru' или 'en' в поле ниже", "ru/en");
+
+if (lang == 'ru') {
+  console.log('Понедельник, вторник, среда, четверг пятницы, суббота, воскресенье.');
+  //     break;
+} else if (lang == 'en') {
+  console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday.');
+  //     break;
 }
+
+//   alert("Неправильное значение. Введите 'ru' или 'en'.");
+// }
+
+
+let map = new Map([
+  ['ru', ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']],
+  ["en", ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn']]
+]);
+
+console.log(map.get(lang));
+
+switch (lang) {
+  case "ru":
+  case "en":
+    console.log(map.get(lang));
+    break;
+
+  default:
+    console.log('Неверное значение');
+    break;
+}
+
+
+
+
+
+
+
+
+
 
 // 2)
 
